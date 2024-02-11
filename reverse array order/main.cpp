58084e8295373a938115583479d13ddfc4222d84
Raufull Islam Rauf <152892595+Rauf-17@users.noreply.github.com>
2024-02-11 14:10:16 +0600

    Add files via upload

---
+++ reverse array order/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+int number[5]={1,2,3,4,5},i;
+
+cout<<"Numbers in reverse order : "<<endl;
+
+for(i=4;i>=0;i--){
+
+cout<<number[i]<<endl;
+
+}
+
+return 0;
+
+}
